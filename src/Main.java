6cb54e96eb71a0f268ed79ab73d4d8446096ef16
AlexKrylov <slate_86@mail.ru>
2017-12-08 15:30:43 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(calculator(3, 20));
+    }
+
+    public static int calculator(int a, int b) {
+        if (a == b) return 1;
+        if (a > b) return 0;
+        return calculator(a + 1, b) + (calculator(a * 2, b));
     }
 }
+
